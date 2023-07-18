5c1a40c45fc49bb05df5d18ee24423751ca6e6e4
Danny <robertodanielalvesduarte@gmail.com>
2023-07-18 16:32:04 +0100

    Demo cleanup.

--- Demo/main.cxx
+++ Demo/main.cxx
@@ -13,18 +13,6 @@ struct Velocity
 
 void main()
 {
-    dbgio_init();
-    dbgio_dev_default_init(DBGIO_DEV_VDP2_ASYNC);
-    dbgio_dev_font_load();
-
-    vdp2_tvmd_display_res_set(VDP2_TVMD_INTERLACE_NONE, VDP2_TVMD_HORZ_NORMAL_A,
-                              VDP2_TVMD_VERT_224);
-
-    vdp2_scrn_back_color_set(VDP2_VRAM_ADDR(3, 0x01FFFE),
-                             RGB1555(1, 0, 3, 3));
-
-    vdp2_tvmd_display_set();
-
     Entity::Create(Position(10, 20), Velocity(1, 2));
 
     while (1)
@@ -41,10 +29,6 @@ void main()
         vdp2_sync();
         vdp2_sync_wait();
     }
-
-    dbgio_flush();
-    vdp2_sync();
-    vdp2_sync_wait();
 }
 
 void user_init()
