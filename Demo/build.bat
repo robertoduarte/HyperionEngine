321c73c1091db6e015be5113c9228b516056c141
Danny <robertodanielalvesduarte@gmail.com>
2024-01-25 14:32:31 +0000

    Updates Demo to use latest yaul-win64 package.

--- Demo/build.bat
+++ Demo/build.bat
@@ -1,7 +1,7 @@
 @ECHO off
 
 IF NOT EXIST yaul/ (
-    powershell -Command "Invoke-Webrequest https://github.com/robertoduarte/yaul-win64/releases/download/v0.3.1-2023.10.15/yaul-0.3.1-win64-20231015.zip -Outfile yaul.zip"
+    powershell -Command "Invoke-Webrequest https://github.com/robertoduarte/yaul-win64/releases/download/v0.3.1-2024.01.25/yaul-0.3.1-win64-20240125.zip -Outfile yaul.zip"
     powershell -Command "Expand-Archive -Force yaul.zip"
     del yaul.zip
 )
